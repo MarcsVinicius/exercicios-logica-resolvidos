255f821e7a61b0c590f701653a0778832c76a011
MarcsVinicius <marcos.vgalencar@gmail.com>
2021-10-13 15:53:33 -0300

    Feito 3 exercicios bloco 3

--- fundamentos/bloco4-introducao-a-java-script/arrays-e-for/exercicio3.js
+++ fundamentos/bloco4-introducao-a-java-script/arrays-e-for/exercicio3.js
@@ -2,8 +2,9 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let result = 0;
 for (let i = 0; i < numbers.length; i++){
     result = numbers[i] + result;
-    console.log(i)
-    if (i == numbers.length){
-        console.log(passou)
+    console.log(result)
+    if (i === numbers.length - 1){
+        result = result / i;
+        console.log(result);
     }
 }
