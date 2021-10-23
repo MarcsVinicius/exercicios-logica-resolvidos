2a2a11528c7eddf56741772364ce383f9bae9497
MarcsVinicius <marcos.vgalencar@gmail.com>
2021-10-23 19:11:03 -0300

    Feito alteraçoes no para-fixar do dia de eventos para praticar novas coisas

--- fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/Eventos/para-fixar/script.js
+++ fundamentos/bloco-5-DOM-Eventos-e-Web-Storage/Eventos/para-fixar/script.js
@@ -1,62 +1,119 @@
-const firstLi = document.getElementById('first-li');
-const secondLi = document.getElementById('second-li');
-const thirdLi = document.getElementById('third-li');
-const input = document.getElementById('input');
-const myWebpage = document.getElementById('my-spotrybefy');
+const firstLi = document.getElementById("first-li");
+const secondLi = document.getElementById("second-li");
+const thirdLi = document.getElementById("third-li");
+const input = document.getElementById("input");
+const myWebpage = document.getElementById("my-spotrybefy");
 
-// 1. Copie esse arquivo e edite apenas ele;
-// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
-// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
-
-// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
-// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-window.onload = function(){
 let lastUp = firstLi;
-function upOnClick(event){
-        lastUp.classList.remove('tech')
-    if(lastUp !== event.target){
-    lastUp = event.target;
-   }
-   event.target.className = "tech";
-}
-firstLi.addEventListener("click",upOnClick)
-secondLi.addEventListener("click",upOnClick)
-thirdLi.addEventListener("click",upOnClick)
+  function upOnClick(event) {
+    lastUp.classList.remove("tech");
+    if (lastUp !== event.target) {
+      lastUp = event.target;
+    }
+    event.target.className = "tech";
+  }
+  firstLi.addEventListener("click", upOnClick);
+  secondLi.addEventListener("click", upOnClick);
+  thirdLi.addEventListener("click", upOnClick);
 
-
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
-
-input.addEventListener('input',function(event){
+  input.addEventListener("input", function (event) {
     lastUp.innerText = input.value;
+    localStorage.setItem(lastUp.id, lastUp.innerText);
+  });
+
+  myWebpage.addEventListener("dblclick", function () {
+    window.open("https://google.com", "_blank");
+  });
+  myWebpage.addEventListener("mouseover", function () {
+    myWebpage.style.color = "#2fc18c";
+  });
+  myWebpage.addEventListener("mouseleave", function () {
+    myWebpage.style.color = "white";
+  });
+
+  function resetText(event) {
+    let target = document.getElementById(event.target.id);
+    target.innerText = "Adicione sua tencnologia favorita";
+    localStorage.setItem(target.id, target.innerText);
+  }
+
+  firstLi.addEventListener("dblclick", resetText);
+  secondLi.addEventListener("dblclick", resetText);
+  thirdLi.addEventListener("dblclick", resetText);
+
+  function addStorage(objeto) {
+    document.getElementById(objeto).innerText = localStorage.getItem(objeto);
+  }
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let chave = localStorage.key(i);
+    if(chave == 'first-li' || chave == 'second-li' || chave == 'third-li')
+    addStorage(chave);
+  }
+
+  const backgroundSelect = document.querySelector(".background-section");
+
+  backgroundSelect.addEventListener("click", function (event) {
+    let selected = event.target;
+    if (selected.id == "blue") {
+      localStorage.setItem("bcg", selected.id);
+      loadBackground()
+    }
+    if (selected.id == "red") {
+      localStorage.setItem("bcg", selected.id);
+      loadBackground()
+    }
+    if (selected.id == "yellow") {
+      localStorage.setItem("bcg", selected.id);
+      loadBackground()
+    }
+    if (selected.id == "green") {
+      localStorage.setItem("bcg", selected.id);
+      loadBackground()
+    }
+  });
+const customBackgroundColor = document.getElementById("customBackgroundColor");
+customBackgroundColor.addEventListener("input", function () {
+  document.body.style.backgroundColor = customBackgroundColor.value;
+  localStorage.setItem("bcg", customBackgroundColor.value)
+});
+
+function loadBackground(){
+  document.body.style.backgroundColor = localStorage.getItem('bcg')
+}
+
+loadBackground()
+//
+const fontSection = document.querySelector('.font-section');
+fontSection.addEventListener('click', function(e){
+  selected = e.target;
+  if(selected.id == "blue"){
+    localStorage.setItem('fntc', selected.id)
+    changeFontColor()
+  }
+  if(selected.id == "red"){
+    localStorage.setItem('fntc', selected.id)
+    changeFontColor()
+  }
+  if(selected.id == "yellow"){
+    localStorage.setItem('fntc', selected.id)
+    changeFontColor()
+  }
+  if(selected.id == "green"){
+    localStorage.setItem('fntc', selected.id)
+    changeFontColor()
+  }
 })
 
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
-// 4.1. Que tal redirecionar para seu portifólio?
-myWebpage.addEventListener('dblclick', function(){
-    window.open('https://google.com', '_blank')
-});
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
-myWebpage.addEventListener('mouseover', function(){
-    myWebpage.style.color = "#2fc18c"
-});
-myWebpage.addEventListener('mouseleave', function(){
-    myWebpage.style.color = "white"
-});
-// Segue abaixo um exemplo do uso de event.target:
+function changeFontColor(){
+  document.body.style.color = localStorage.getItem('fntc')
+  firstLi.style.color = localStorage.getItem('fntc')
 }
 
-function resetText(event) {
-  // O Event é passado como um parâmetro para a função.
-  event.target.innerText = 'Opção reiniciada';
-  // O event possui várias propriedades, porém a mais usada é o event.target,
-  // que retorna o objeto que disparou o evento.
-}
+const inputcolor = document.getElementById("inputcolor");
+inputcolor.addEventListener("input", function () {
+  localStorage.setItem("fntc", inputcolor.value)
+  changeFontColor()
+});
 
-firstLi.addEventListener('dblclick', resetText);
-
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
+changeFontColor()
