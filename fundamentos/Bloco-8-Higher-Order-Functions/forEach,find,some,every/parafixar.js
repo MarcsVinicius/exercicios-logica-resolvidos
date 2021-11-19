fb5e2a6f89a5f95147e94b4a610555c6ffad76ae
MarcsVinicius <marcos.vgalencar@gmail.com>
2021-11-18 22:45:09 -0300

    exercicios do dia

---
+++ fundamentos/Bloco-8-Higher-Order-Functions/forEach,find,some,every/parafixar.js
@@ -0,0 +1,78 @@
+const books = [
+    {
+      id: 1,
+      name: 'As Crônicas de Gelo e Fogo',
+      genre: 'Fantasia',
+      author: {
+        name: 'George R. R. Martin',
+        birthYear: 1948,
+      },
+      releaseYear: 1991,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: {
+        name: 'J. R. R. Tolkien',
+        birthYear: 1892,
+      },
+      releaseYear: 1954,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Isaac Asimov',
+        birthYear: 1920,
+      },
+      releaseYear: 1951,
+    },
+    {
+      id: 4,
+      name: 'Duna',
+      genre: 'Ficção Científica',
+      author: {
+        name: 'Frank Herbert',
+        birthYear: 1920,
+      },
+      releaseYear: 1965,
+    },
+    {
+      id: 5,
+      name: 'A Coisa',
+      genre: 'Terror',
+      author: {
+        name: 'Stephen King',
+        birthYear: 1947,
+      },
+      releaseYear: 1986,
+    },
+    {
+      id: 6,
+      name: 'Oi',
+      genre: 'Terror',
+      author: {
+        name: 'H. P. Lovecraft',
+        birthYear: 1890,
+      },
+      releaseYear: 1928,
+    },
+  ];
+
+  //console.log(books.find((pessoa)=> pessoa.author.birthYear === 1947).author.name);
+//   let nameBook =  books[0].name;
+//   books.forEach((value) => {
+//       if(value.name.length < nameBook.length){
+//           nameBook = value.name;
+//       }
+//   })
+
+  //console.log(books.find((book) => book.name.length === 26).name)
+
+
+  function booksOrderedByReleaseYearDesc() {
+	return books.sort((a, b) => a.releaseYear - b.releaseYear);
+}
+console.log(booksOrderedByReleaseYearDesc());
